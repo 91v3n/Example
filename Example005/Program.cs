deb34921310bafde607934fc4e54d19b957c152a
given <given@live.ru>
2022-09-13 23:21:06 +0600

    Example 005 Masha

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Uahuuu, it's Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
